608a2ad079529e4118117c8251b059bbb37eaa66
funfetus <funfetus@gmail.com>
2022-11-28 19:04:30 -0800

    fix playerInput loop

--- rps-odin.js
+++ rps-odin.js
@@ -1,83 +1,177 @@
-let protag = "Kesh"
+game();
 
-console.log(`${protag} is the protagonist of Aimless Whisper`);
+function game() {
+    options = ['rock', 'paper', 'scissors'];
+    const maxRound = 5;
+    let playerWins = 0;
+    let computerWins = 0;
+    let roundNumber = 1; 
 
-// all of the arrays
-const orgType = [];
-const fundSrc = ["galactic megabank", "international mercenaries", "a shadowy government operative", "representatives of the old way", "a cruel dictator", "an orbital warlord"];
-const orgs = [ "Legion", "Circle", "Commanders", "Raiders"];
-const themes = [ "Wolves", "The Hawk", "The Claw", "The Chain", "Shackles"];
-const adj = [ "Blue", "Black", "Red", "Crimson", "Velvet", "Iron", "Bloody"];
-const noun = [ "Wolf", "Hound", "Saint", "Priest", "King", "Queen", "Prince", "Boss", "Hand", "Blade", "Flame"];
-const charTrait = [ "fastidious", "vicious", "calculating", "beautiful", "terrifying", "psychotic", "charming", "unpredicably violent", "disarmingly meek", "brash", "confident", "vengeful", "aggressive", "seductive", "oddly disarming", "cheerful"];
-const corpNameFirst = ["Con", "Securi", "Multi", "Globo", "Sene", "Cali", "Fina", ];
-const corpNameSecond = ["corp", "con", "cor", "skelion", "della", ];
-const genre = ['mystery', 'blockbuster action', 'thriller', 'horror', 'detective', 'samurai', 'ninja', 'western', 'drama', 'comedy', 'suspense', 'political', 'generational', 'family', 'business'];
-const attackType = ['electro', 'laser', 'pulse', 'ionic', 'psionic', 'neuro', 'sonic'];
-const weaponType = ['gun', 'knife', 'dagger', 'greatsword', 'blaster'];
-const transformType = ['reducer', 'expander', 'dissassembler'];
+     /* loops through this while roundNumber is 5 or less... */
+    while (roundNumber <= maxRound) {
+        let computerSelection = getComputerChoice();
+        let playerSelection = getPlayerInput();
+        let roundResult = playRound(playerSelection, computerSelection);
+        switch(roundResult) {
+            case 'win':
+                console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+                playerWins = (playerWins + 1);
+                roundNumber = (roundNumber + 1)
+            case 'lose':
+                console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+                computerWins = (computerWins + 1);
+                roundNumber = (roundNumber + 1);
+            case 'draw':
+                alert('It\'s a draw! Go again!');
+                console.log('It\'s a draw! Go again!'); /* doesn't increment round number or wins */
+            } 
+        }   
 
-// the workhorse combiner function
-function combiner(arg1, arg2) {
-    let combination = (arg1[Math.floor(Math.random() * arg1.length)] + //get an item from the array designated arg1, and add...
-        arg2[Math.floor(Math.random() * arg2.length)]);  //...an item from the array designated arg2.
-    return combination; // return a combined variable called "combination".
-}
-
-// combiner with a space between the words
-function combinerSpace(arg1, arg2) {
-    let combination = (arg1[Math.floor(Math.random() * arg1.length)] + " " + //get an item from the array designated arg1, and add...
-        arg2[Math.floor(Math.random() * arg2.length)]);  //...an item from the array designated arg2.
-    return combination; // return a combined variable called "combination".
-}
-
-// combiner with "the" between the words
-function combinerThe(arg1, arg2) {
-    let combination = (arg1[Math.floor(Math.random() * arg1.length)] + " the " + //get an item from the array designated arg1, and add...
-        arg2[Math.floor(Math.random() * arg2.length)]);  //...an item from the array designated arg2.
-    return combination; // return a combined variable called "combination".
-}
-
-let chapterGenre = (genre[Math.floor(Math.random() * genre.length)]) + "-" + 
-(genre[Math.floor(Math.random() * genre.length)]);
-
-let antagOrg = (orgs[Math.floor(Math.random() * orgs.length)]) + " of " + 
-    (themes[Math.floor(Math.random() * themes.length)]);
-
-// let antagOrg = ((combiner(combiner(adj, noun), combiner(charTrait, noun)));
+         /* then goes here, for the ending. */
+        if (playerWins > computerWins) { 
+            console.log('Congratulations! You win!');
+        } else {
+            console.log('Sorry, you lost!')
+        }
 
 
-let antagLeader = (adj[Math.floor(Math.random() * orgs.length)]) + " " + (noun[Math.floor(Math.random() * noun.length)]);
+    
+    
+/* ============== defining all the functions here =============== 
+due to "lexical scope", variable availability is determined not by where a function 
+is called from, but by where the function is ORIGINALLY DECLARED. */
 
-let story1 = (`In this chapter, ${protag} takes on the ${antagOrg}.`);
-
-let story2 = (`The leader of ${antagOrg} is ${antagLeader},`);
-
-// let corpName = combiner(corpNameFirst, corpNameSecond);
-
-// let corpName = combiner(combiner(adj, noun), combiner(charTrait, noun));
-
-// let corpName = (combiner(combiner(adj, noun)) + combiner(charTrait, noun));
-
-// let story3 = (`The corporation involved is ${corpName}.`);
-
-function getArrayElement(array) {  //gets an array element from a specified array.
-    return (array[Math.floor(Math.random() * array.length)]);  
+    function playRound(playerSelection, computerSelection) {
+        let result; 
+        switch(computerSelection) {  // switch based on computerSelection
+            case 'rock': {
+                switch(playerSelection) {
+                    case 'rock':
+                        result = 'draw';
+                        break;
+                    case 'paper':
+                        result = 'win';
+                        break;
+                    case 'scissors':
+                        result = 'lose';
+                        break;
+                }
+            }
+            case 'paper': {
+                switch(playerSelection) {
+                    case 'rock':
+                        result = 'win';
+                        break;
+                    case 'paper':
+                        result = 'draw';
+                        break;
+                    case 'scissors':
+                        result = 'lose';
+                        break;
+                }
+            }
+            case 'scissors': {
+                switch(playerSelection) {
+                    case 'rock':
+                        result = 'win';
+                        break;
+                    case 'paper':
+                        result = 'lose';
+                        break;
+                    case 'scissors':
+                        result = 'draw';
+                        break;
+                }
+            }
+        }
+        return result;
     }
 
+    
+    
+    function getArrayElement(array) {  //gets an array element from a specified array.
+        return (array[Math.floor(Math.random() * array.length)]);  
+        }
+
+    function getComputerChoice() {
+        let getChoice = getArrayElement(options);
+        console.log(`computerSelection is: ${getChoice}`);
+        return getChoice;    
+    }
+
+    function getPlayerInput() { //get the player's selection
+        let loopInput = true;
+        while (loopInput == true) {
+            let playerInput = prompt(`Let\'s play a game of rock paper scissors, muff. Best of ${maxRound}. Round ${roundNumber}! Put your choice right down there.`)
+            console.log(playerInput); // for debugging purposes
+            let playerSelection = playerInput.toLowerCase(); // convert the input to lowercase, for case-insensitivity
+            console.log(`playerSelection is: ${playerSelection}`);  // for debugging purposes
+            loopInput = false;
+            if (options.includes(playerSelection) == false) {
+                loopInput = true;
+                console.log('Invalid player input, dumbo.'); // for debugging purposes
+                // alert('That\'s not a choice, stupid. Don\'t you know how to play this game? Pick again!');
+                // playerInput = prompt(`Let\'s play a game of rock paper scissors, muff. Best of ${maxRound}. Round ${roundNumber}! Put your choice right down there.`)
+            }
+        }    
+        return playerSelection;
+        }
+    
+        function checkSelection (selection) { // check if playerSelection is present in the options array
+            if (options.includes(selection)) { // this may be too granular a function...
+                return true;
+            } else {
+                return false;
+            }
+        }
+    
 
 
 
+    // function compareSelection(playerSelection, computerSelection) { // compare the two selections and see who wins!
+    //     let result; 
+    //     switch(computerSelection) {  // switch based on computerSelection
+    //         case 'rock': {
+    //             switch(playerSelection) {
+    //                 case 'rock':
+    //                     result = 'draw';
+    //                     break;
+    //                 case 'paper':
+    //                     result = 'win';
+    //                     break;
+    //                 case 'scissors':
+    //                     result = 'lose';
+    //                     break;
+    //             }
+    //         }
+    //         case 'paper': {
+    //             switch(playerSelection) {
+    //                 case 'rock':
+    //                     result = 'win';
+    //                     break;
+    //                 case 'paper':
+    //                     result = 'draw';
+    //                     break;
+    //                 case 'scissors':
+    //                     result = 'lose';
+    //                     break;
+    //             }
+    //         }
+    //         case 'scissors': {
+    //             switch(playerSelection) {
+    //                 case 'rock':
+    //                     result = 'win';
+    //                     break;
+    //                 case 'paper':
+    //                     result = 'lose';
+    //                     break;
+    //                 case 'scissors':
+    //                     result = 'draw';
+    //                     break;
+    //             }
+    //         }
+    //     }
+    //     return result;
+    // }
 
-// let vilCharTrait1 = (charTrait[Math.floor(Math.random() * charTrait.length)]);
-// let vilCharTrait2 = (charTrait[Math.floor(Math.random() * charTrait.length)]);
-// while (vilCharTrait1 == vilCharTrait2) {  // check if the vilCharTraits are the same, and re-do if necessary
-//     vilCharTrait2 = (charTrait[Math.floor(Math.random() * charTrait.length)]);
-// }
-
-let story3 = `The ${getArrayElement(charTrait)} and ${getArrayElement(charTrait)} ${(getArrayElement(adj +' ' +noun ))}`;
-
-// let story4 = `the corpname is (`${(combiner(adj, noun) + (combiner(charTrait, noun)))}`;
-let story4 = `The genre of this chapter is ${chapterGenre}.`;
-
-
+} //end game function
